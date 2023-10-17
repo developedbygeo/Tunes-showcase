b602c072c2a2d2dc232963b126c051ecca11ea99
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-17 19:31:31 +0200

    chore: sanity config

---
+++ sanity.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'sanity';
+import { deskTool } from 'sanity/desk';
+import { visionTool } from '@sanity/vision';
+import { schemaTypes } from './schemas';
+
+export default defineConfig({
+  name: 'default',
+  title: 'dj-portfolio',
+
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string,
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool()],
+  basePath: '/studio',
+
+  schema: {
+    types: schemaTypes,
+  },
+});
