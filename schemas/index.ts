8954b4dbe6e21a9ffd5607e53a9996a3b3251b11
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-20 01:33:06 +0200

    chore: registering schemas

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,4 +1,9 @@
 import events from '@/schemas/events';
 import albums from '@/schemas/albums';
 
-export const schemaTypes = [events, albums];
+import author from '@/schemas/blog/author';
+import category from '@/schemas/blog/category';
+import blockContent from '@/schemas/blog/blockContent';
+import blogPost from '@/schemas/blog/blogPost';
+
+export const schemaTypes = [events, albums, author, category, blogPost, blockContent];
