d15bd46ee0e41a8be5bc61cf13ed7096592cbc28
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-21 23:33:35 +0200

    chore: track file title update

--- schemas/albums.ts
+++ schemas/albums.ts
@@ -1,62 +1,62 @@
 import { Rule } from 'sanity';
 
 const albums = {
-  name: 'album',
-  title: 'Album',
-  type: 'document',
-  fields: [
-    {
-      name: 'id',
-      title: 'ID',
-      type: 'string',
-      validation: (Rule: Rule) => Rule.required(),
-    },
-    {
-      name: 'img',
-      title: 'Image',
-      type: 'image',
-      validation: (Rule: Rule) => Rule.required(),
-    },
-    {
-      name: 'name',
-      title: 'Name',
-      type: 'string',
-      validation: (Rule: Rule) => Rule.required(),
-    },
-    {
-      name: 'tracks',
-      title: 'Tracks',
-      type: 'array',
-      of: [
+    name: 'album',
+    title: 'Album',
+    type: 'document',
+    fields: [
         {
-          name: 'track',
-          title: 'Track',
-          type: 'object',
-          fields: [
-            {
-              name: 'name',
-              title: 'Name',
-              type: 'string',
-              validation: (Rule: Rule) => Rule.required(),
-            },
-            {
-              name: 'track',
-              title: 'Track',
-              type: 'file',
-              validation: (Rule: Rule) => Rule.required(),
-            },
-          ],
+            name: 'id',
+            title: 'ID',
+            type: 'string',
+            validation: (Rule: Rule) => Rule.required(),
         },
-      ],
-      validation: (Rule: Rule) => Rule.required(),
+        {
+            name: 'img',
+            title: 'Image',
+            type: 'image',
+            validation: (Rule: Rule) => Rule.required(),
+        },
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string',
+            validation: (Rule: Rule) => Rule.required(),
+        },
+        {
+            name: 'tracks',
+            title: 'Tracks',
+            type: 'array',
+            of: [
+                {
+                    name: 'track',
+                    title: 'Track',
+                    type: 'object',
+                    fields: [
+                        {
+                            name: 'name',
+                            title: 'Name',
+                            type: 'string',
+                            validation: (Rule: Rule) => Rule.required(),
+                        },
+                        {
+                            name: 'file',
+                            title: 'Track File',
+                            type: 'file',
+                            validation: (Rule: Rule) => Rule.required(),
+                        },
+                    ],
+                },
+            ],
+            validation: (Rule: Rule) => Rule.required(),
+        },
+    ],
+    preview: {
+        select: {
+            title: 'name',
+            subtitle: 'tracks.length',
+        },
     },
-  ],
-  preview: {
-    select: {
-      title: 'name',
-      subtitle: 'tracks.length',
-    },
-  },
 };
 
 export default albums;
