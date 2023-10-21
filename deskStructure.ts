c85eb9fb4aaf4c1ac7025c1ad3754b94665baf9c
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-21 23:15:10 +0200

    chore: adding desk icons

--- deskStructure.ts
+++ deskStructure.ts
@@ -1,4 +1,7 @@
 import { deskTool } from 'sanity/desk';
+import deskIcons from '@/common/deskIcons';
+
+import { BsCalendar2Event } from 'react-icons/bs';
 
 const customDeskStructure = () =>
   deskTool({
@@ -8,25 +11,27 @@ const customDeskStructure = () =>
         .items([
           S.listItem()
             .id('general')
+            .icon(deskIcons.general.icon)
             .title('General')
             .child(
               S.list()
                 .title('General Content')
                 .items([
-                  S.documentTypeListItem('event').title('Events'),
-                  S.documentTypeListItem('album').title('Albums'),
+                  S.documentTypeListItem('event').title('Events').icon(deskIcons.general.events),
+                  S.documentTypeListItem('album').title('Albums').icon(deskIcons.general.albums),
                 ])
             ),
           S.listItem()
+            .icon(deskIcons.blog.icon)
             .id('blog')
             .title('Blog')
             .child(
               S.list()
                 .title('Blog Content')
                 .items([
-                  S.documentTypeListItem('author').title('Authors'),
-                  S.documentTypeListItem('category').title('Categories'),
-                  S.documentTypeListItem('blogPost').title('Blog Posts'),
+                  S.documentTypeListItem('author').title('Authors').icon(deskIcons.blog.authors),
+                  S.documentTypeListItem('category').title('Categories').icon(deskIcons.blog.categories),
+                  S.documentTypeListItem('blogPost').title('Blog Posts').icon(deskIcons.blog.posts),
                 ])
             ),
         ]),
