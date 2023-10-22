49ecd2e1cbbdfa25148f58e356a1fe576672fe90
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-22 22:17:13 +0200

    chore: adding event type

--- types/sanity/index.ts
+++ types/sanity/index.ts
@@ -1,7 +1,30 @@
 export type BlogPostSelection = {
-  title: string;
-  author: string;
-  media: string;
-  date: string;
-  description: string;
+    title: string;
+    author: string;
+    media: string;
+    date: string;
+    description: string;
+};
+
+export type Event = {
+    _id: string;
+    date: {
+        day: string;
+        month: string;
+    };
+    location: {
+        city: string;
+        country: string;
+        address: string;
+    };
+    price: string;
+};
+
+export type Album = {
+    _id: string;
+    name: string;
+    tracks: {
+        name: string;
+        assetUrl: string;
+    }[];
 };
