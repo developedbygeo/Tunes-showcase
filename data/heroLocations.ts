bdf66948092a6f1c209d6614f17683911be3f7a7
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-22 22:17:36 +0200

    chore: adding sequence for hero animation

---
+++ data/heroLocations.ts
@@ -0,0 +1,16 @@
+const heroLocationSeq = [
+    'Los Angeles, USA',
+    5000,
+    'Miami, USA',
+    5000,
+    'Rio de Janeiro, Brazil',
+    5000,
+    'Berlin, Germany',
+    5000,
+    'Paris, France',
+    5000,
+    'Athens, Greece',
+    5000,
+];
+
+export default heroLocationSeq;
