12ad45d2a80ef46fb14e512101118e31f4f47e85
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-28 14:47:20 +0200

    refactor: adding call to cms

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
+import { getHero } from '@/libs/sanity/utils';
+
 import Events from '@/components/modules/Events/Events';
-import Hero from '@/components/modules/Hero';
+import Hero from '@/components/modules/Hero/Hero';
 
 const Home = async () => {
+    const data = await getHero();
     return (
         <main className="flex min-h-screen flex-col items-center">
             <h1 className="sr-only">DJ Cat Paws</h1>
-            <Hero className="mb-44" />
+            <Hero data={data} className="mb-44" />
             <Events />
             <div className="h-[4000px] w-screen"></div>
         </main>
