da6a76c3ad742357bae4cf5981183dc322cedb9c
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-28 14:47:37 +0200

    chore: adding cms icons

--- common/deskIcons.tsx
+++ common/deskIcons.tsx
@@ -1,25 +1,29 @@
 import {
-  MdViewModule,
-  MdEvent,
-  MdAlbum,
-  MdOutlineCategory,
-  MdCollectionsBookmark,
-  MdOutlineModeEdit,
-  MdNewspaper,
+    MdStream,
+    MdOutlineCatchingPokemon,
+    MdViewModule,
+    MdEvent,
+    MdAlbum,
+    MdOutlineCategory,
+    MdCollectionsBookmark,
+    MdOutlineModeEdit,
+    MdNewspaper,
 } from 'react-icons/md';
 
 const deskIcons = {
-  general: {
-    icon: MdViewModule,
-    events: MdEvent,
-    albums: MdAlbum,
-  },
-  blog: {
-    icon: MdCollectionsBookmark,
-    authors: MdOutlineModeEdit,
-    categories: MdOutlineCategory,
-    posts: MdNewspaper,
-  },
+    general: {
+        icon: MdViewModule,
+        header: MdStream,
+        hero: MdOutlineCatchingPokemon,
+        events: MdEvent,
+        albums: MdAlbum,
+    },
+    blog: {
+        icon: MdCollectionsBookmark,
+        authors: MdOutlineModeEdit,
+        categories: MdOutlineCategory,
+        posts: MdNewspaper,
+    },
 };
 
 export default deskIcons;
