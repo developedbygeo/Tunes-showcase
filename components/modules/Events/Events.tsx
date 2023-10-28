ed0b51ad59be5fb1e33d9b549af78f756e66e522
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-28 14:48:26 +0200

    refactor: rendering client components

--- components/modules/Events/Events.tsx
+++ components/modules/Events/Events.tsx
@@ -1,18 +1,16 @@
 import { getEvents } from '@/libs/sanity/utils';
-import { cn } from '@/libs/utils';
+import { cn } from '@/libs/ui';
 import { WithClassName } from '@/types/UI';
 
 import EventsList from '@/components/modules/Events/EventsList';
+import EventsHeader from './EventsHeader';
 
 const Events = async ({ className }: WithClassName) => {
     const data = await getEvents();
 
     return (
         <section id="events" className={cn('container flex flex-col gap-8 xl:gap-12', className)}>
-            <div className="w-full text-center">
-                <h2 className="big-title">World Tour Alert</h2>
-                <p className="subtitle mt-0 xl:mt-4">Join the most purrfect tour!</p>
-            </div>
+            <EventsHeader />
             <EventsList data={data} />
         </section>
     );
