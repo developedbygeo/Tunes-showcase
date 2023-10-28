36191d8beba087a60866c722c6cf37dfcb508860
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-28 14:47:53 +0200

    feat: updating header

--- components/modules/Header.tsx
+++ components/modules/Header.tsx
@@ -1,5 +1,14 @@
-const Header = () => {
-    return <div>HEADER</div>;
+import { getHeader } from '@/libs/sanity/utils';
+import AnimatedHeader from '@/components/modules/AnimatedHeader';
+
+const Header = async () => {
+    const headerData = await getHeader();
+    console.log(headerData.navigation[0]);
+    return (
+        <header>
+            <AnimatedHeader links={headerData.navigation} src={headerData.logo} />
+        </header>
+    );
 };
 
 export default Header;
