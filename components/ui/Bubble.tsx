833d9134201315a6ed57449e6bfaed1014acdd4a
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-30 22:01:01 +0100

    chore: import update

--- components/ui/Bubble.tsx
+++ components/ui/Bubble.tsx
@@ -1,6 +1,6 @@
 import { CSSProperties } from 'react';
 
-import { cn } from '@/libs/utils';
+import { cn } from '@/libs/ui';
 import { WithClassName } from '@/types/UI';
 
 type BubbleProps = WithClassName & {
