8eea787f6569d852d08c9fe0fda8971e6e2b186b
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-31 23:53:18 +0100

    chore: schema update

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,3 +1,6 @@
+import header from '@/schemas/header';
+import footer from '@/schemas/footer';
+import hero from '@/schemas/hero';
 import events from '@/schemas/events';
 import albums from '@/schemas/albums';
 
@@ -5,5 +8,17 @@ import author from '@/schemas/blog/author';
 import category from '@/schemas/blog/category';
 import blockContent from '@/schemas/blog/blockContent';
 import blogPost from '@/schemas/blog/blogPost';
+import musicDescription from './musicDescription';
 
-export const schemaTypes = [events, albums, author, category, blogPost, blockContent];
+export const schemaTypes = [
+    header,
+    footer,
+    hero,
+    events,
+    albums,
+    musicDescription,
+    author,
+    category,
+    blogPost,
+    blockContent,
+];
