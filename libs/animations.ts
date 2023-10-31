164634f6686d2bd52400fca81dc79df62aa5115d
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-31 23:52:54 +0100

    style: adding more helpers and constants

--- libs/animations.ts
+++ libs/animations.ts
@@ -1,5 +1,8 @@
 import { AnimationDirection } from '@/types/UI';
 
+export const SEQUENCE_DELAY = 3000;
+export const VINYL_ROTATION = 360 * 20;
+
 export const fadeIn = (direction: AnimationDirection, delay: number) => ({
     hidden: {
         y: direction === 'up' ? 80 : direction === 'down' ? -80 : 0,
@@ -20,6 +23,40 @@ export const fadeIn = (direction: AnimationDirection, delay: number) => ({
             type: 'tween',
             duration: 1.35,
             delay,
+            ease: [0.2, 0.2, 0.2, 0.175],
+        },
+    },
+});
+
+export const fadeInChildren = (direction: AnimationDirection, delay: number, childrenDelay: number) => {
+    const fadeEffect = fadeIn(direction, delay);
+    return {
+        ...fadeEffect,
+        visible: {
+            ...fadeEffect.visible,
+            transition: {
+                ...fadeEffect.visible.transition,
+                staggerChildren: childrenDelay,
+            },
+        },
+    };
+};
+
+// having a delay in a child cancels out the staggerChildren from the parent.
+export const simpleFadeInNoDirection = () => ({
+    hidden: {
+        opacity: 0,
+        transition: {
+            type: 'tween',
+            duration: 1.5,
+            ease: [0.25, 0.6, 0.3, 0.8],
+        },
+    },
+    visible: {
+        opacity: 1,
+        transition: {
+            type: 'tween',
+            duration: 1.35,
             ease: [0.2, 0.2, 0.2, 0.175],
         },
     },
