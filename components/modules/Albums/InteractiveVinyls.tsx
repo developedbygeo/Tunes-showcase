7e4acdbdb9acf638c542e9efcfa11f21b032ead4
developedbygeo <georgiosanagnostou@hotmail.com>
2023-11-01 01:05:56 +0100

    chore: updating vinyl rotation

--- components/modules/Albums/InteractiveVinyls.tsx
+++ components/modules/Albums/InteractiveVinyls.tsx
@@ -7,7 +7,7 @@ import Vinyl1 from '@/assets/vinyl1.svg';
 import Vinyl2 from '@/assets/vinyl2.svg';
 import Vinyl3 from '@/assets/vinyl3.svg';
 
-import { VINYL_ROTATION, fadeIn, fadeInChildren } from '@/libs/animations';
+import { VINYL_ROTATION, fadeIn, fadeInChildren, fadeInWithRotation } from '@/libs/animations';
 
 const InteractiveVinyls = ({ className }: WithClassName) => {
     return (
@@ -18,17 +18,17 @@ const InteractiveVinyls = ({ className }: WithClassName) => {
             className="relative h-[60vh] w-[30vw]"
         >
             <motion.div
-                variants={fadeIn('left', 0.45)}
+                variants={fadeInWithRotation('left', 0.45, 360)}
                 animate="visible"
                 initial="hidden"
                 className="absolute left-0 top-0"
-                whileHover={{ rotate: VINYL_ROTATION }}
+                whileHover={{ rotate: -VINYL_ROTATION }}
                 transition={{ duration: 50, ease: 'linear', loop: Infinity }}
             >
                 <Vinyl1 className="h-60 w-60 rounded-full shadow-container-neumorph" />
             </motion.div>
             <motion.div
-                variants={fadeIn('left', 0.65)}
+                variants={fadeInWithRotation('left', 0.65, 180)}
                 animate="visible"
                 initial="hidden"
                 className="-translate-1/2 absolute bottom-1/2 left-1/2"
@@ -38,11 +38,11 @@ const InteractiveVinyls = ({ className }: WithClassName) => {
                 <Vinyl2 className="h-60 w-60 rounded-full shadow-container-neumorph" />
             </motion.div>
             <motion.div
-                variants={fadeIn('left', 0.79)}
+                variants={fadeInWithRotation('left', 0.79, 55)}
                 animate="visible"
                 initial="hidden"
                 className="absolute bottom-8 right-12"
-                whileHover={{ rotate: VINYL_ROTATION }}
+                whileHover={{ rotate: -VINYL_ROTATION }}
                 transition={{ duration: 22, ease: 'linear', loop: Infinity }}
             >
                 <Vinyl3 className="h-60 w-60 rounded-full shadow-container-neumorph" />
