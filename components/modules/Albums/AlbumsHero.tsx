9a9d508950832bcfd745d8e43c1e7ba22f6a7aa4
developedbygeo <georgiosanagnostou@hotmail.com>
2023-11-01 01:05:36 +0100

    chore: adding cn

--- components/modules/Albums/AlbumsHero.tsx
+++ components/modules/Albums/AlbumsHero.tsx
@@ -7,6 +7,7 @@ import InteractiveVinyls from '@/components/modules/Albums/InteractiveVinyls';
 import { Button } from '@/components/ui/Button';
 
 import { fadeIn } from '@/libs/animations';
+import { cn } from '@/libs/ui';
 
 const AlbumsHero = ({ className }: WithClassName) => {
     const target = useRef<HTMLDivElement>(null);
@@ -31,7 +32,7 @@ const AlbumsHero = ({ className }: WithClassName) => {
             ref={target}
             className="relative isolate overflow-hidden bg-transparent before:pointer-events-none before:fixed before:inset-0 before:z-0 before:bg-[radial-gradient(circle_farthest-side_at_var(--x,_1rem)_var(--y,_1rem),_#7987FF_0%,_transparent_100%)] before:opacity-40"
         >
-            <div className="apply-pt container flex items-center justify-between px-0">
+            <div className={cn('apply-pt apply-pb container flex items-center justify-between px-0', className)}>
                 <motion.div
                     variants={fadeIn('default', 0.3)}
                     initial="hidden"
