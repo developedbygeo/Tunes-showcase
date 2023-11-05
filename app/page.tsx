ed9164787bb9fc780d11af183972513dfba6b239
developedbygeo <georgiosanagnostou@hotmail.com>
2023-11-05 19:20:47 +0100

    style: removing min-h

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import Hero from '@/components/modules/Hero/Hero';
 const Home = async () => {
     const data = await getHero();
     return (
-        <div className="flex min-h-screen flex-col items-center">
+        <div className="flex flex-col items-center">
             <h1 className="sr-only">DJ Cat Paws</h1>
-            <Hero data={data} className="mb-44" />
+            <Hero data={data} />
         </div>
     );
 };
