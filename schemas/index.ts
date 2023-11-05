a35df7366abdfae92d5c8b10e6cf0a6addd13a9d
developedbygeo <georgiosanagnostou@hotmail.com>
2023-11-05 19:22:24 +0100

    chore: adding biography

--- schemas/index.ts
+++ schemas/index.ts
@@ -8,12 +8,14 @@ import author from '@/schemas/blog/author';
 import category from '@/schemas/blog/category';
 import blockContent from '@/schemas/blog/blockContent';
 import blogPost from '@/schemas/blog/blogPost';
-import musicDescription from './musicDescription';
+import musicDescription from '@/schemas/musicDescription';
+import biography from '@/schemas/biography';
 
 export const schemaTypes = [
     header,
     footer,
     hero,
+    biography,
     events,
     albums,
     musicDescription,
