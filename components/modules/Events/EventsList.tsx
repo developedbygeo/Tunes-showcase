6404fe3c63c6faf93f1e79126c35ae490a6b2caa
developedbygeo <georgiosanagnostou@hotmail.com>
2023-11-06 22:58:17 +0100

    chore: cleanup

--- components/modules/Events/EventsList.tsx
+++ components/modules/Events/EventsList.tsx
@@ -1,16 +1,14 @@
 'use client';
 
+import { useRef } from 'react';
 import Image from 'next/image';
 import { motion, useInView, useScroll, useTransform } from 'framer-motion';
 import { CiLocationOn } from 'react-icons/ci';
 
 import { Event } from '@/types/sanity';
-import { fadeIn } from '@/libs/animations';
+import { WithClassName } from '@/types/UI';
 
 import { Button } from '@/components/ui/Button';
-import { WithClassName } from '@/types/UI';
-import { cn } from '@/libs/ui';
-import { useRef } from 'react';
 
 type EventsListProps = WithClassName & {
     data: Event[];
@@ -46,14 +44,14 @@ const EventsList = ({ className, data }: EventsListProps) => {
                 ref={targetRef}
                 className="relative z-10 mt-[-30vh] flex h-[95vh] flex-col items-center justify-center py-20"
             >
-                <motion.div
+                <motion.section
                     ref={extendedRef}
                     className="container mb-8 text-center"
                     style={{ scale: contentScale, opacity: contentOpacity }}
                 >
                     <h2 className="big-title text-shadow-hero-header">World Tour Alert</h2>
                     <p className="subtitle mt-0 xl:mt-4">Join the most purrfect tour!</p>
-                </motion.div>
+                </motion.section>
                 <motion.div
                     className="container flex h-[40rem] origin-top flex-col justify-between gap-x-4 overflow-y-auto px-0 xl:flex-row"
                     style={{ scale: contentScale, opacity: contentOpacity }}
@@ -68,93 +66,48 @@ const EventsList = ({ className, data }: EventsListProps) => {
                             alt="DJ Cat Paws on tour"
                         />
                     </div>
-                    <motion.ul
-                        className="c-scrollbar flex w-80 flex-1 flex-col justify-between scroll-smooth shadow-container-neumorph"
+                    <motion.section
                         style={{ overflowY: listScrollBehavior }}
+                        className="c-scrollbar flex w-80 flex-1 flex-col justify-between scroll-smooth shadow-container-neumorph"
                     >
-                        {data.map((event) => (
-                            <li
-                                className="flex flex-col items-center justify-between gap-y-4 border-b border-white/10 px-0 py-8 text-center xl:my-0 xl:flex-row xl:gap-y-0 xl:px-6 xl:py-10 xl:text-left"
-                                key={event._id}
-                            >
-                                <div className="flex flex-col items-center gap-x-6 xl:flex-row">
-                                    <time
-                                        className="mb-4 flex flex-col items-center justify-center leading-tight text-gray-300 xl:mb-0"
-                                        dateTime={`${event.date.day}-${event.date.month}`}
-                                    >
-                                        <p className="font-anton text-3xl font-bold">{event.date.day}</p>
-                                        <p className="text-lg">{event.date.month}</p>
-                                    </time>
-                                    <article className="flex w-64 flex-col justify-start gap-y-2">
-                                        <h3 className="sr-only h-0">Location: {event.location.city}</h3>
-                                        <p className="text-lg font-bold leading-none xl:whitespace-nowrap">
-                                            {event.location.city}, {event.location.country}
-                                        </p>
-                                        <div className="flex items-center justify-center gap-x-1 xl:justify-start">
-                                            <CiLocationOn className="h-6 w-6 text-accent/90" />
-                                            <p className="font-light">{event.location.address}</p>
-                                        </div>
-                                    </article>
-                                </div>
-                                <p className="mt-2 w-full text-center text-sm font-bold text-accent">{event.price}</p>
-                                <Button className="mt-2 xl:mt-0">Get tickets</Button>
-                            </li>
-                        ))}
-                    </motion.ul>
+                        <h3 className="sr-only">Current locations on tour</h3>
+                        <ul>
+                            {data.map((event) => (
+                                <li
+                                    className="flex flex-col items-center justify-between gap-y-4 border-b border-white/10 px-0 py-8 text-center xl:my-0 xl:flex-row xl:gap-y-0 xl:px-6 xl:py-10 xl:text-left"
+                                    key={event._id}
+                                >
+                                    <div className="flex flex-col items-center gap-x-6 xl:flex-row">
+                                        <time
+                                            className="mb-4 flex flex-col items-center justify-center leading-tight text-gray-300 xl:mb-0"
+                                            dateTime={`${event.date.day}-${event.date.month}`}
+                                        >
+                                            <p className="font-anton text-3xl font-bold">{event.date.day}</p>
+                                            <p className="text-lg">{event.date.month}</p>
+                                        </time>
+                                        <article className="flex w-64 flex-col justify-start gap-y-2">
+                                            <h3 className="sr-only h-0">Location: {event.location.city}</h3>
+                                            <p className="text-lg font-bold leading-none xl:whitespace-nowrap">
+                                                {event.location.city}, {event.location.country}
+                                            </p>
+                                            <div className="flex items-center justify-center gap-x-1 xl:justify-start">
+                                                <CiLocationOn className="h-6 w-6 text-accent/90" />
+                                                <p className="font-light">{event.location.address}</p>
+                                            </div>
+                                        </article>
+                                    </div>
+                                    <p className="mt-2 w-full text-center text-sm font-bold text-accent">
+                                        {event.price}
+                                    </p>
+                                    <Button className="mt-2 xl:mt-0">Get tickets</Button>
+                                </li>
+                            ))}
+                        </ul>
+                    </motion.section>
                 </motion.div>
             </motion.div>
         </>
     );
-
-    // const ref = useRef<HTMLDivElement | null>(null);
-    // const isInView = useInView(ref);
-
-    // return (
-    //     <motion.div
-    //         ref={ref}
-    //         variants={fadeIn('up', 0.4)}
-    //         initial="hidden"
-    //         animate={isInView ? 'visible' : 'hidden'}
-    //         whileInView="visible"
-    //         viewport={{ once: true }}
-    //         className="flex h-[35rem] flex-col justify-between gap-x-4 overflow-y-auto xl:flex-row"
-    //     >
-    //         <div className="hidden w-[22rem] xl:flex">
-    //             <Image src="/tour.jpg" width={358} height={489} priority quality={100} alt="DJ Cat Paws on tour" />
-    //         </div>
-    //         <ul className="c-scrollbar flex w-80 flex-1 flex-col justify-between overflow-y-auto scroll-smooth shadow-container-neumorph">
-    //             {data.map((event) => (
-    //                 <li
-    //                     className="flex flex-col items-center justify-between gap-y-4 border-b border-white/10 px-0 py-8 text-center xl:my-0 xl:flex-row xl:gap-y-0 xl:px-6 xl:py-10 xl:text-left"
-    //                     key={event._id}
-    //                 >
-    //                     <div className="flex flex-col items-center gap-x-6 xl:flex-row">
-    //                         <time
-    //                             className="mb-4 flex flex-col items-center justify-center leading-tight text-gray-300 xl:mb-0"
-    //                             dateTime={`${event.date.day}-${event.date.month}`}
-    //                         >
-    //                             <p className="font-anton text-3xl font-bold">{event.date.day}</p>
-    //                             <p className="text-lg">{event.date.month}</p>
-    //                         </time>
-
-    //                         <article className="flex w-64 flex-col justify-start gap-y-2">
-    //                             <h3 className="sr-only h-0">Location: {event.location.city}</h3>
-    //                             <p className="text-lg font-bold leading-none xl:whitespace-nowrap">
-    //                                 {event.location.city}, {event.location.country}
-    //                             </p>
-    //                             <div className="flex items-center justify-center gap-x-1 xl:justify-start">
-    //                                 <CiLocationOn className="h-6 w-6 text-accent/90" />
-    //                                 <p className="font-light">{event.location.address}</p>
-    //                             </div>
-    //                         </article>
-    //                     </div>
-    //                     <p className="mt-2 w-full text-center text-sm font-bold text-accent">{event.price}</p>
-    //                     <Button className="mt-2 xl:mt-0">Get tickets</Button>
-    //                 </li>
-    //             ))}
-    //         </ul>
-    //     </motion.div>
-    // );
 };
 
 export default EventsList;
