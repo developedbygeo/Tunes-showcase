a3fe143eed1637fe29568ee9a01d275829d3c151
developedbygeo <georgiosanagnostou@hotmail.com>
2023-11-08 00:45:48 +0100

    chore: adding image and preview

--- schemas/blog/blogPost.ts
+++ schemas/blog/blogPost.ts
@@ -2,79 +2,85 @@ import { BlogPostSelection } from '@/types/sanity';
 import { Rule } from 'sanity';
 
 const blogPost = {
-  name: 'blogPost',
-  title: 'Blog Post',
-  type: 'document',
-  fields: [
-    {
-      name: 'title',
-      title: 'Title',
-      type: 'string',
-      validation: (Rule: Rule) => Rule.required(),
+    name: 'blogPost',
+    title: 'Blog Post',
+    type: 'document',
+    fields: [
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'image',
+            validation: (Rule: Rule) => Rule.required(),
+        },
+        {
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+            validation: (Rule: Rule) => Rule.required(),
+        },
+        {
+            name: 'id',
+            title: 'ID',
+            type: 'string',
+            validation: (Rule: Rule) => Rule.required(),
+        },
+        {
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: {
+                source: 'title',
+                maxLength: 96,
+            },
+            validation: (Rule: Rule) => Rule.required(),
+        },
+        {
+            name: 'author',
+            title: 'Author',
+            type: 'reference',
+            to: [{ type: 'author' }],
+            validation: (Rule: Rule) => Rule.required(),
+        },
+        {
+            name: 'categories',
+            title: 'Categories',
+            type: 'array',
+            of: [{ type: 'reference', to: [{ type: 'category' }] }],
+            validation: (Rule: Rule) => Rule.required(),
+        },
+        {
+            name: 'publishedAt',
+            title: 'Published At',
+            type: 'datetime',
+            validation: (Rule: Rule) => Rule.required(),
+        },
+        {
+            name: 'description',
+            title: 'Description',
+            type: 'text',
+            validation: (Rule: Rule) => Rule.required(),
+        },
+        {
+            name: 'body',
+            title: 'Body',
+            type: 'blockContent',
+            validation: (Rule: Rule) => Rule.required(),
+        },
+    ],
+    preview: {
+        select: {
+            title: 'title',
+            author: 'author.name',
+            media: 'image',
+            date: 'publishedAt',
+            description: 'description',
+        },
+        prepare: (selection: BlogPostSelection) => {
+            const { author, date } = selection;
+            const ddmmyyyy = new Date(date).toLocaleDateString('en-GB');
+            return { ...selection, subtitle: author && `by ${author} - ${ddmmyyyy}` };
+        },
     },
-    {
-      name: 'id',
-      title: 'ID',
-      type: 'string',
-      validation: (Rule: Rule) => Rule.required(),
-    },
-    {
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
-      options: {
-        source: 'title',
-        maxLength: 96,
-      },
-      validation: (Rule: Rule) => Rule.required(),
-    },
-    {
-      name: 'author',
-      title: 'Author',
-      type: 'reference',
-      to: [{ type: 'author' }],
-      validation: (Rule: Rule) => Rule.required(),
-    },
-    {
-      name: 'categories',
-      title: 'Categories',
-      type: 'array',
-      of: [{ type: 'reference', to: [{ type: 'category' }] }],
-      validation: (Rule: Rule) => Rule.required(),
-    },
-    {
-      name: 'publishedAt',
-      title: 'Published At',
-      type: 'datetime',
-      validation: (Rule: Rule) => Rule.required(),
-    },
-    {
-      name: 'description',
-      title: 'Description',
-      type: 'text',
-      validation: (Rule: Rule) => Rule.required(),
-    },
-    {
-      name: 'body',
-      title: 'Body',
-      type: 'blockContent',
-      validation: (Rule: Rule) => Rule.required(),
-    },
-  ],
-  preview: {
-    select: {
-      title: 'title',
-      author: 'author.name',
-      media: 'mainImage',
-      date: 'publishedAt',
-      description: 'description',
-    },
-    prepare: (selection: BlogPostSelection) => {
-      const { author, date } = selection;
-      const ddmmyyyy = new Date(date).toLocaleDateString('en-GB');
-      return { ...selection, subtitle: author && `by ${author} - ${ddmmyyyy}` };
-    },
-  },
 };
 
 export default blogPost;
