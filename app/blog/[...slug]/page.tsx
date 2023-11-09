caceb75d1e1d76314df101a3e07b988fc212ab72
developedbygeo <georgiosanagnostou@hotmail.com>
2023-11-09 23:17:27 +0100

    feat: adding cathc-all slug route

---
+++ app/blog/[...slug]/page.tsx
@@ -0,0 +1,19 @@
+import { notFound } from 'next/navigation';
+import { getBlogPost } from '@/libs/sanity/utils';
+
+import Post from '@/components/modules/Blog/Post';
+
+const BlogPost = async ({ params }: { params: { slug: string } }) => {
+    const postData = await getBlogPost(params.slug);
+
+    if (!postData) return notFound();
+
+    return (
+        <section className="apply-py container">
+            <h1 className="sr-only">{postData.title}</h1>
+            <Post data={postData} />
+        </section>
+    );
+};
+
+export default BlogPost;
