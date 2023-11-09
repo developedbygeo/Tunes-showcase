d8e817cdf3a3d0dc6b9397e7d495a23b771d22fa
developedbygeo <georgiosanagnostou@hotmail.com>
2023-11-09 23:17:57 +0100

    feat: blog homepage

---
+++ components/modules/Blog/Post.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import Image from 'next/image';
+import { PortableText } from '@portabletext/react';
+
+import { cn } from '@/libs/ui';
+import { WithClassName } from '@/types/UI';
+import { BlogPostData } from '@/types/sanity';
+
+import ScrollToTopButton from '@/components/modules/ScrollToTop';
+
+type PostProps = WithClassName & {
+    data: BlogPostData;
+};
+
+const Post = ({ className, data }: PostProps) => {
+    return (
+        <article className={cn('flex flex-col gap-6', className)}>
+            <ScrollToTopButton />
+            <h1 className="text-3xl font-semibold">{data.title}</h1>
+            <div className="relative aspect-square max-h-[50rem] w-full">
+                <Image src={data.image} alt={data.description} fill />
+            </div>
+            <div className="portable-text mt-8">
+                <PortableText value={data.body} />
+            </div>
+        </article>
+    );
+};
+
+export default Post;
