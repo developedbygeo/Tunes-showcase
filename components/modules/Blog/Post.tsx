e47f6978b51f642405c6c7a4564ef1d91e701ddc
developedbygeo <georgiosanagnostou@hotmail.com>
2023-11-19 14:48:38 +0100

    Add back button to blog post

--- components/modules/Blog/Post.tsx
+++ components/modules/Blog/Post.tsx
@@ -1,13 +1,16 @@
 'use client';
 
+import Link from 'next/link';
 import Image from 'next/image';
 import { PortableText } from '@portabletext/react';
+import { SlArrowLeft } from 'react-icons/sl';
 
 import { cn } from '@/libs/ui';
 import { WithClassName } from '@/types/UI';
 import { BlogPostData } from '@/types/sanity';
 
 import ScrollToTopButton from '@/components/modules/ScrollToTop';
+import { Button } from '@/components/ui/Button';
 
 type PostProps = WithClassName & {
     data: BlogPostData;
@@ -16,6 +19,12 @@ type PostProps = WithClassName & {
 const Post = ({ className, data }: PostProps) => {
     return (
         <article className={cn('flex flex-col gap-6', className)}>
+            <Button asChild variant="link" className="relative left-0 my-4 self-start">
+                <Link className="flex gap-4 !text-gray-400 hover:!text-white" href="/blog">
+                    <SlArrowLeft className="h-4 w-4" />
+                    <span>Back to posts</span>
+                </Link>
+            </Button>
             <ScrollToTopButton />
             <h1 className="text-3xl font-semibold">{data.title}</h1>
             <div className="relative aspect-square max-h-[30rem] w-full lg:max-h-[50rem]">
